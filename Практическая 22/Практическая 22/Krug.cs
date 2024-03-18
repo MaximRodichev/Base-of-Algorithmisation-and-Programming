0fb4e7661cda2e177c37529bdcd2e24698eb3985
Maxim Rodichev <vymasfyaya@gmail.com>
2024-03-19 01:59:10 +0300

    Практическая 22

---
+++ Практическая 22/Практическая 22/Krug.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_22
+{
+    class OKrug
+    {
+        private protected double Radius_;
+        public double Radius
+        {
+            get { return Radius_; }
+            set { 
+                if (value < 0) { throw new Exception("Radius is negative"); }
+                else { Radius_ = value; }
+            }
+        }
+
+        public OKrug(double k)
+        {
+            this.Radius = k;
+        }
+        public double S()
+        {
+            return Radius * 2 * Math.PI;
+        }
+        public void Info()
+        {
+            Console.WriteLine($"Okrujnost'\nRadius: {Radius}\nS: {S()}");
+        }
+
+    }
+
+    class Zillinder : OKrug
+    {
+        private double Height_;
+        public double Height
+        {
+            get { return Height_; }
+            set
+            {
+                if (value < 0) { throw new Exception("Height is negative"); }
+                else { Height_ = value; }
+            }
+        }
+
+        public Zillinder(double Radius, double Height) : base(Radius)
+        {
+            this.Height = Height;
+        }
+        public Zillinder(OKrug a, double Height) : base(a.Radius)
+        {
+            this.Radius = a.Radius;
+            this.Height = Height;
+        }
+        public double S()
+        {
+            return base.S() * Height;
+        }
+        public double V()
+        {
+            return Math.PI * Radius * Radius * Height;
+        }
+        public void Info()
+        {
+            Console.WriteLine($"Zillinder\nRadius: {Radius}\nS: {S()}");
+            Console.WriteLine($"V: {V()}");
+        }
+    }
+}
---
+++ Практическая 22/Практическая 22/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OKrug b = new OKrug(5);
+            Zillinder c = new Zillinder(b, 20);
+            Zillinder a = new Zillinder(10, 10);
+
+            b.Info();
+            c.Info();
+            a.Info();
+            Console.ReadLine();
+
+        }
+    }
+}
